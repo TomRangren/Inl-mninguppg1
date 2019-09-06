9a8a19599ad7e15b19084b0abc37fd2967a38fcb
TomRangren <https://github.com/TomRangren/Inl-mninguppg1.git>
2019-09-06 09:50:13 +0200

    La till lite kodning

---
+++ Inlämningsuppg1/Inlämningsuppg1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Inlämningsuppg1
+{
+    class Program
+    {
+        static void Main()
+        {
+            RandomGenerator generator = new Random();
+            int rand = generator.Next(1, 100);
+            Console.WriteLine(rand);
+            Console.WriteLine("Gissa ett tal mellan 1-100!: ");
+            if (svar == rand);
+
+
+
+
+
+
+
+        }
+    }
+}
